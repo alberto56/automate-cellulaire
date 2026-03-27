5283b7dad99c3f4a3edea2959c9e7165009a4625
Albert Albala <albert@mediatribe.net>
2026-03-26 23:25:33 -0400

    2026-03-26-23:25:33

---
+++ docs/extra/controllers/Builder.js
@@ -0,0 +1,16 @@
+class Builder extends Controller {
+  init() {
+    for (let col = 0; col < this.model.cols; col++) {
+      for (let row = 0; row < this.model.rows; row++) {
+        this.setAlive(col, row, false);
+      }
+    }
+    return super.init();
+  }
+  setAlive(col, row, alive) {
+    this.model.set(col, row, 'alive', alive);
+    this.model.set(col, row, 'r', alive ? this.config.alive.r : this.config.dead.r);
+    this.model.set(col, row, 'g', alive ? this.config.alive.g : this.config.dead.g);
+    this.model.set(col, row, 'b', alive ? this.config.alive.b : this.config.dead.b);
+  }
+}
